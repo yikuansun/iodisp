3dcc885a1c91f4b5cb3662cdac7946c940d63a39
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-10-15 20:58:02 -0400

    Remap specific keys

--- app/code.js
+++ app/code.js
@@ -1,8 +1,15 @@
 const { uIOhook, UiohookKey } = require("uiohook-napi");
+const fs = require("fs");
 
 var keymapInverted = {};
 for (var keyName in UiohookKey) {
     var keyRaw = UiohookKey[keyName];
     keymapInverted[keyRaw] = keyName;
+}
+
+var remapping = JSON.parse(fs.readFileSync(__dirname + "/remap.json", "utf-8"));
+for (var keyRaw in keymapInverted) {
+    var keyName = keymapInverted[keyRaw];
+    if (remapping[keyName]) keymapInverted[keyRaw] = remapping[keyName];
 }
 console.log(keymapInverted);
