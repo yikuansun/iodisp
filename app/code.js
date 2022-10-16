dd0df2117421082379cccaa4f49351f66d5d3efb
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-10-15 20:54:33 -0400

    Generate key map

--- app/code.js
+++ app/code.js
@@ -1,1 +1,8 @@
 const { uIOhook, UiohookKey } = require("uiohook-napi");
+
+var keymapInverted = {};
+for (var keyName in UiohookKey) {
+    var keyRaw = UiohookKey[keyName];
+    keymapInverted[keyRaw] = keyName;
+}
+console.log(keymapInverted);
